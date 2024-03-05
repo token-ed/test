1c61fd12b763ae9778f08886ed434f8b9b936e36
Ed <emiguel.dev@gmail.com>
2024-03-05 13:17:11 +0100

    Test repo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="bg-slate-500/30 backdrop-blur-sm border-black border-2 rounded-sm">
+          {" "}
+          Heloo
+        </div>
+      </body>
     </html>
   );
 }
